23cf9d0f613914a569b8c0ae8489732f55ed956c
Aavoqato2 <sadettin.stanfel@gmail.com>
2026-05-13 13:02:47 +0200

    * Added Playermovement WASD & Jump(Spacebar) TODO: GroundController & Implementign DoubleJump

---
+++ Assets/_scripts/player/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    private float _speed;
+    [SerializeField]
+    private float _jumpSpeed;
+    private PlayerInputController _playerInputController;
+    private Rigidbody _rigidbody;
+    private bool _jumpTriggered;
+
+    private void Awake()
+    {
+        _playerInputController = GetComponent<PlayerInputController>();
+        _rigidbody = GetComponent<Rigidbody>();
+        
+        _playerInputController.OnJumpButtonPressed += JumpButtonPressed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = new Vector3(_playerInputController.MovementInputVector.x, 0, _playerInputController.MovementInputVector.y)* _speed;
+        velocity.y = _rigidbody.linearVelocity.y;
+        if(_jumpTriggered)
+        {
+            velocity.y = _jumpSpeed;
+            _jumpTriggered = false;
+        }
+        _rigidbody.linearVelocity = velocity;
+    }
+    
+    private void JumpButtonPressed()
+    {
+        _jumpTriggered = true;
+    }
+}

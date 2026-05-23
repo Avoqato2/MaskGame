fee079fcd7cabbcee125a542f7a9297a3ae55302
Avoqato2 <mt241087@fhstp.ac.at>
2026-05-23 14:58:57 +0200

    Camra offset fix

--- Assets/_scripts/Camera/CameraController.cs
+++ Assets/_scripts/Camera/CameraController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [Header("Camera Settings")]
-    [SerializeField] private GameObject _player;
+    [SerializeField] private Transform _playerTransform;
     [SerializeField] private float _cameraHeight = 12.5f;
     private Vector3 _offSet;
 	
@@ -14,6 +14,6 @@ public class CameraController : MonoBehaviour
     void LateUpdate()
     {
         //camera Position vom Player + Offset
-        transform.position = new Vector3(_player.transform.position.x + _offSet.x,_cameraHeight, _player.transform.position.z + _offSet.z);
+        transform.position = new Vector3(_playerTransform.position.x + _offSet.x,_cameraHeight +_playerTransform.position.y, _playerTransform.position.z + _offSet.z);
     }
 }
